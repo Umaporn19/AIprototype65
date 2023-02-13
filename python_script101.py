3683f373d1d5b8472125c2fb5b12237ec018027c
Umaporn19 <Umaporn_kpc@kkumail.com>
2023-02-13 15:12:17 +0700

    basic python py

--- python_script101.py
+++ python_script101.py
@@ -10,7 +10,7 @@ def parse_input():
     # parser.add_argument(
     #     'n', 
     #     type=int,
-    #     help='value of M positional argument')
+    #     help='value of N positional argument')    
 
     parser.add_argument(
         '--x', 
@@ -31,7 +31,7 @@ def print_other():
     print('something else')
 
 def print_ones():
-    print('1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ')
+    print('1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1')
 
 if __name__ == "__main__":
     x = parse_input()
@@ -44,14 +44,6 @@ if __name__ == "__main__":
     # print(f'm = {x.m}')
     # print(f'n = {x.n}')
 
-# 
-
-
-
-
-
-
-#if __name__ == "__main__":
 
     # args = parse_input()
     
